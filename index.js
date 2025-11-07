7260832ce203b3d89b1320b52869d0730f425328
oliullahakib <oliullahakib@gmail.com>
2025-11-07 21:04:50 +0600

    model delete api added

--- index.js
+++ index.js
@@ -70,6 +70,12 @@ async function run() {
             const result = await modelCollection.updateOne(query,update)
             res.send(result)
         })
+        app.delete('/models/:id',async(req,res)=>{
+            const id = req.params.id
+            const query = {_id:new ObjectId(id)}
+            const result = await modelCollection.deleteOne(query)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
