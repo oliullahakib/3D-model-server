d8b1d46076a892ece93b934d49519e8c9f376b83
oliullahakib <oliullahakib@gmail.com>
2025-11-07 18:42:14 +0600

    initial the server and added recent,details and models api with mongodb connection

---
+++ index.js
@@ -0,0 +1,64 @@
+// dependance
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// public api 
+app.get('/', (req, res) => {
+    res.send("server is running")
+})
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wfr9cox.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const db = client.db("3D-model")
+        const modelCollection = db.collection("model")
+        // model api 
+        app.get('/recent-model',async(req, res) => {
+            const result = await modelCollection.find().sort({ created_at:-1}).limit(6).toArray()
+            res.send(result)
+        })
+        app.get('/model-details/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)}
+            const result = await modelCollection.findOne(query)
+            res.send(result)
+        })
+        app.post('/models',async(req,res)=>{
+            const newModel=req.body;
+            const result = await modelCollection.insertOne(newModel)
+            res.send(result)
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
