8944d33f3ad46b19ac4881b4dba00369a7c88986
oliullahakib <oliullahakib@gmail.com>
2025-11-08 10:07:54 +0600

    added api for my model and use firebase sdk auth with middleware

--- index.js
+++ index.js
@@ -3,17 +3,44 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+var admin = require("firebase-admin");
+var serviceAccount = require("./firebase-sdk-key.json");
 
 const app = express();
 const port = process.env.PORT || 3000
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 // middleware
 app.use(cors())
 app.use(express.json())
+// custom middleware 
+const verifyFirebaseToken = async(req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.status(401).send({ message: "Unauthorize access" })
+    }
+    const token = authorization.split(' ')[1]
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorize access" })
+    }
+    // vrifyToken 
+    try {
+        const decode = await admin.auth().verifyIdToken(token)
+        req.token_email = decode.email
+        next()
+    } catch {
+        return res.status(401).send({ message: "Unauthorize access" })
+    }
 
+}
 // public api 
 app.get('/', (req, res) => {
     res.send("server is running")
+    req.headers
 })
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wfr9cox.mongodb.net/?appName=Cluster0`;
 
@@ -35,44 +62,55 @@ async function run() {
         const modelCollection = db.collection("model")
         // model api 
 
-        app.get('/models',async(req, res) => {
-            const query = req.query.category;
-            console.log(query)
+        app.get('/models', async (req, res) => {
+            const result = await modelCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/my-models', verifyFirebaseToken, async (req, res) => {
+            const category = req.query.category;
+            const email = req.query.email;
+            if(req.token_email!==email){
+                return res.status(403).send("Forbidden Access")
+            }
             let filter = {}
-            if(query){
-                filter = {category:query}
+            if (category) {
+                filter = { category: category }
+            }
+            if (email) {
+                filter = { created_by: email }
             }
             const result = await modelCollection.find(filter).toArray()
             res.send(result)
         })
-        app.get('/recent-model',async(req, res) => {
-            const result = await modelCollection.find().sort({ created_at:-1}).limit(6).toArray()
+
+        app.get('/recent-model', async (req, res) => {
+            const result = await modelCollection.find().sort({ created_at: -1 }).limit(6).toArray()
             res.send(result)
         })
-        app.get('/model-details/:id',async(req,res)=>{
+        app.get('/model-details/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await modelCollection.findOne(query)
             res.send(result)
         })
-        app.post('/models',async(req,res)=>{
-            const newModel=req.body;
+        app.post('/models', async (req, res) => {
+            const newModel = req.body;
             const result = await modelCollection.insertOne(newModel)
             res.send(result)
         })
-        app.put('/models/:id',async(req,res)=>{
+        app.put('/models/:id', async (req, res) => {
             const id = req.params.id
-            const newModel=req.body
-            const query = {_id:new ObjectId(id)}
+            const newModel = req.body
+            const query = { _id: new ObjectId(id) }
             const update = {
-                $set:newModel
+                $set: newModel
             }
-            const result = await modelCollection.updateOne(query,update)
+            const result = await modelCollection.updateOne(query, update)
             res.send(result)
         })
-        app.delete('/models/:id',async(req,res)=>{
+        app.delete('/models/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id:new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await modelCollection.deleteOne(query)
             res.send(result)
         })
